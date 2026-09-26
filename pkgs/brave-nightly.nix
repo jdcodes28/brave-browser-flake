19ac8c289ec5c76b1e96501a850824e68b5abaa2
jdcodes28 <114588726+jdcodes28@users.noreply.github.com>
2026-09-26 08:54:09 +0000

    chore: update brave versions

--- pkgs/brave-nightly.nix
+++ pkgs/brave-nightly.nix
@@ -5,8 +5,8 @@
   vulkanSupport ? false,
 }:
 let
-  version = "1.98.30";
-  hash = "0v73p13jspiyp89y100f68p67h63bai8z1c1qn42i3719z5y8v3w";
+  version = "1.98.33";
+  hash = "0hlmbjrlqwz2advznl5pf2f6h1a00997xppmvn33p5f33zzkrwf1";
 in
 callPackage ./build-brave.nix { inherit vulkanSupport; } {
   pname = "brave-nightly";
